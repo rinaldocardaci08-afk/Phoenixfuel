3c981c40b0b51f8d91b12df2ad078b8b60e197d2
rinaldocardaci08-afk <rinaldocardaci08@gmail.com>
2026-03-17 00:50:46 +0100

    Generatore listino prezzi PDF con marginalità

--- app.js
+++ app.js
@@ -1609,6 +1609,10 @@ function apriConfermaOrdine(ordineId) {
   window.open('conferma_ordine.html?ordine_id=' + ordineId, '_blank');
 }
 
+function apriListinoPDF() {
+  window.open('listino_pdf.html', '_blank');
+}
+
 async function apriDettaglioCarico(caricoId) {
   const { data: carico } = await sb.from('carichi').select('*, carico_ordini(sequenza, ordini(cliente,prodotto,litri,note))').eq('id', caricoId).single();
   if (!carico) return;
